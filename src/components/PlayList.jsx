d957f6d18ba5e3603119db89f7ac3854a61ff401
victor13bars <victor_bars_bars95@mail.ru>
2022-12-16 15:51:38 +0300

    ContextMenu - open logic.

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PlayListContextMenu from "./PlayListContextMenu";
 import PlayListCover from "./PlayListCover";
 import PlayListButtonPlay from "./PlayListButtonPlay";
@@ -29,20 +29,33 @@ const menuItems = [
 ]
 
 const PlayList = ({coverUrl, title, description, classes}) => {
+
+    const [isContextMenuOpen, setIsContextMenuOpen] = useState(false)
+    const openContextMenu = (event) => {
+        event.preventDefault()
+        setIsContextMenuOpen(true)
+    }
+
     return (
         <a
             href="/"
-           className={`relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group ${classes}`}>
+            className={`relative p-4 rounded-md bg-[#181818] hover:bg-[#272727] duration-200 group ${classes}`}
+            onContextMenu={openContextMenu}
+        >
             <div className="relative">
                 <PlayListCover url={coverUrl}/>
                 <PlayListButtonPlay/>
             </div>
             <PlayListTitle title={title}/>
             <PlayListDescription description={description}/>
-            <PlayListContextMenu
-                menuItems={menuItems}
-                classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10 hidden group-hover:block"
-            />
+            {
+                isContextMenuOpen
+                &&
+                <PlayListContextMenu
+                    menuItems={menuItems}
+                    classes="absolute top-9 left-9 bg-[#282828] text-[#eaeaea] text-sm divide-y divide-[#3e3e3e] p-1 rounded shadow-xl cursor-default whitespace-nowrap z-10"
+                />
+            }
         </a>
     );
 };
