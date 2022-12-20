14dba792eeb3359e2f8ab3034b29588bce27e680
victor13bars <victor_bars_bars95@mail.ru>
2022-12-20 14:30:56 +0300

    dynamic movement ContextMenu.

--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -1,10 +1,9 @@
-import React, {useLayoutEffect, useRef, useState} from 'react';
+import React, {useEffect, useLayoutEffect, useRef, useState} from 'react';
 import PlayListContextMenu from "./PlayListContextMenu";
 import PlayListCover from "./PlayListCover";
 import PlayListButtonPlay from "./PlayListButtonPlay";
 import PlayListTitle from "./PlayListTitle";
 import PlayListDescription from "./PlayListDescription";
-import {useEffect} from "@types/react";
 
 const menuItems = [
     {
@@ -46,9 +45,30 @@ const PlayList = ({coverUrl, title, description, classes, toggleScrolling}) => {
     const closeContextMenu = () => {
         setIsContextMenuOpen(false)
     }
+
+    const updateContextMenuHorizontalPosition = () => {
+
+        const menuWidth = contextMenuRef.current.offsetWidth
+        const shouldMoveLeft = menuWidth > window.innerWidth - clickPosition.x
+
+        contextMenuRef.current.style.left = shouldMoveLeft
+            ? `${clickPosition.x - menuWidth}px`
+            : `${clickPosition.x}px`
+    }
+
+    const updateContextMenuVerticalPosition = () => {
+
+        const menuHeight = contextMenuRef.current.offsetHeight
+        const shouldMoveUp = menuHeight > window.innerHeight - clickPosition.y
+
+        contextMenuRef.current.style.top = shouldMoveUp
+            ? `${clickPosition.y - menuHeight}px`
+            : `${clickPosition.y}px`
+    }
+
     const updateContextMenuPosition = () => {
-        contextMenuRef.current.style.top = `${clickPosition.y}px`
-        contextMenuRef.current.style.left = `${clickPosition.x}px`
+        updateContextMenuHorizontalPosition()
+        updateContextMenuVerticalPosition()
     }
 
     useLayoutEffect(() => {
