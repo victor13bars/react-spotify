07579e0672fcd483dc938766e949d517d9eac183
victor13bars <victor_bars_bars95@mail.ru>
2023-01-06 14:33:04 +0300

    hide BasePopover.

--- src/components/BaseButton.jsx
+++ src/components/BaseButton.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const BaseButton = ({primary, children: label, classes}) => {
+const BaseButton = ({primary, children: label, classes, onClick: handleClick}) => {
 
     const typeClasses = primary ? 'bg-white hover:bg-gray-100 text-[#2e2e2e]' : 'text-white'
 
     return (
         <button
+            onClick={handleClick}
             className={`font-semibold leading-5 py-[9px] px-[17px]
              sm:px-[38px] rounded-full hover:scale-105 ${typeClasses} ${classes}`}
         >
--- src/components/BasePopover.jsx
+++ src/components/BasePopover.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import BaseButton from "./BaseButton";
 
 const BasePopover = () => {
+
+    const ref = useRef()
+    const [classes, setClasses] = useState()
+
+    const hide = () => {
+        setClasses('opacity-0 pointer-events-none')
+    }
+
+    useEffect(() => {
+
+        const handleClickAway = ({target}) => {
+            if (!ref.current.contains(target)) hide()
+        }
+
+        document.addEventListener('mousedown', handleClickAway)
+
+        return () => document.removeEventListener('mousedown', handleClickAway)
+
+    })
+
     return (
         <div
-            className="fixed top-[227px] left-[200px] z-30 bg-[#0e72ea] text-white tracking-wide
-             rounded-lg shadow-3xl p-4 min-w-[330px] select-none">
+            ref={ref}
+            className={`fixed top-[227px] left-[200px] z-30 bg-[#0e72ea] text-white tracking-wide
+             rounded-lg shadow-3xl p-4 min-w-[330px] transition duration-300 select-none ${classes}`}>
             <h3 className="text-lg font-bold mb-2">Create a playlist</h3>
             <p className="text-xs">Log in to create and share playlist</p>
             <div className="mt-6 text-right">
-                <BaseButton>NOT NOW</BaseButton>
-                <BaseButton primary>LOG IN</BaseButton>
+                <BaseButton onClick={hide}>Not now</BaseButton>
+                <BaseButton primary>Log in</BaseButton>
             </div>
             <div
                 className="w-20 h-20 absolute -top-4 -left-20 flex justify-end items-center overflow-hidden pointer-events-none">
