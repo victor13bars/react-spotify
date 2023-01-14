304af30b2c1ca0f4c12a846e2a0dca81bf0c140b
victor13bars <victor_bars_bars95@mail.ru>
2023-01-14 16:25:18 +0300

    BaseModal closing logic.

--- src/components/BaseModal.jsx
+++ src/components/BaseModal.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {XMarkIcon} from '@heroicons/react/24/outline'
 
 const BaseModal = ({onClose: handleClose}) => {
+
+    useEffect(() => {
+
+        const handleEsc = ({key}) => {
+            if (key === 'Escape') handleClose()
+        }
+
+        document.addEventListener('keydown', handleEsc)
+
+        return () => document.removeEventListener('keydown', handleEsc)
+    })
+
     return (
-        <div className='fixed inset-0 bg-black/70 z-30 flex justify-center items-center' role='dialog'>
-            <div className='relative bg-[#333] h-1/3 w-2/5 rounded-xl'>
+        <div
+            className='fixed inset-0 bg-black/70 z-30 flex justify-center items-center' role='dialog'
+            onClick={handleClose}
+        >
+            <div
+                className='relative bg-[#333] h-1/3 w-2/5 rounded-xl'
+                onClick={(event) => event.stopPropagation()}
+            >
                 <button
                     className='absolute right-0 p-3 text-neutral-500 hover:text-neutral-200'
                     onClick={handleClose}
