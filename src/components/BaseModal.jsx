640a574f642c8ced27ce626177e23ddda1578701
victor13bars <victor_bars_bars95@mail.ru>
2023-01-15 22:25:17 +0300

    BaseModal opening animation.

--- src/components/BaseModal.jsx
+++ src/components/BaseModal.jsx
@@ -1,9 +1,17 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {XMarkIcon} from '@heroicons/react/24/outline'
 
 const BaseModal = ({onClose: handleClose}) => {
 
+    const ref = useRef()
+    const contentRef = useRef()
+
     useEffect(() => {
+
+        setTimeout(() => {
+            ref.current.classList.remove('opacity-0')
+            contentRef.current.classList.remove('-translate-y-10')
+        }, 500)
 
         const handleEsc = ({key}) => {
             if (key === 'Escape') handleClose()
@@ -16,12 +24,15 @@ const BaseModal = ({onClose: handleClose}) => {
 
     return (
         <div
-            className='fixed inset-0 bg-black/70 z-30 flex justify-center items-center' role='dialog'
+            className='fixed inset-0 bg-black/70 z-30 flex justify-center items-center opacity-0 transition-opacity duration-500'
+            role='dialog'
+            ref={ref}
             onClick={handleClose}
         >
             <div
-                className='relative bg-[#333] h-1/3 w-2/5 rounded-xl'
+                className='relative bg-[#333] h-1/3 w-2/5 rounded-xl -translate-y-10 transition-transform duration-500'
                 onClick={(event) => event.stopPropagation()}
+                ref={contentRef}
             >
                 <button
                     className='absolute right-0 p-3 text-neutral-500 hover:text-neutral-200'
